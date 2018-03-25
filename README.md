574ffaaf1416ee9ced0aae2bbd88763846712015
André Luiz Leoni <andreluizleoni@gmail.com>
2018-03-25 12:25:09 -0300

    Altorithm versions

---
+++ README.md
@@ -0,0 +1,4 @@
+## Run with
+docker run -ti --rm -p 8888:8888 -v "$PWD":/home/jovyan/work andreleoni/jupyter-tensorflow
+
+Get the URL and paste on the navigator
---
+++ single_prediction/v1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from keras.preprocessing import image
+
+# Single prediction analysis
+test_image = image.load_img(prediction_url, target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+
+if result[0][0] == 1:
+  prediction = 'correct'
+else:
+  prediction = 'false'
